7298fd4839ff9a41fd525d31554af76b2483835a
友人Ｓ <YuuJinS@users.noreply.github.com>
2022-05-30 19:03:40 +0900

    Add new trigger

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,41 +3,50 @@ module.exports = {
 	once: false,
 	async execute(message) {
 		if (message.author.bot) return;
-		if
-		(
+		if (
+			//  日文觸發詞
 			message.content === 'サラおはよう' ||
-			//message.content === '祭おはよう' ||
-			message.content === 'サラ早安' //||
-			//message.content === '祭早安'
+			message.content === 'おはよう' ||
+			// 中文觸發詞
+			message.content === '星川早安' ||
+			message.content === '早安' ||
+			// 中日混和觸發詞
+			message.content === '星川おはよう' ||
+			message.content === 'サラ早安'
 		) {
 			const RandomMorning =
 			[
-				'おはよう！',
-				'早安呀～',
+				'おはよう～♪',
+				'早安呀～♪',
 			];
 			return message.reply(
 				{
 					allowedMentions: { repliedUser: false },
-					content: `${RandomMorning[Math.floor(Math.random() * RandomMorning.length)]}`,
+					content: `${interaction.user.name}${RandomMorning[Math.floor(Math.random() * RandomMorning.length)]}`,
 					ephemeral: false,
 				},
 			);
 		}
 		if (
+			//  日文觸發詞
 			message.content === 'サラおやすみ' ||
-			//message.content === '祭おやすみ' ||
-			message.content === 'サラ晚安' //||
-			//message.content === '祭晚安'
+			message.content === 'おやすみ' ||
+			// 中文觸發詞
+			message.content === '星川晚安' ||
+			message.content === '晚安',
+			// 中日混和觸發詞
+			message.content === '星川おやすみ' ||
+			message.content === 'サラ晚安'
 		) {
 			const RandomMorning =
 			[
-				'おやすみ！',
-				'晚安呀～',
+				'おやすみ～♪',
+				'晚安呀～♪',
 			];
 			return message.reply(
 				{
 					allowedMentions: { repliedUser: false },
-					content: `${RandomMorning[Math.floor(Math.random() * RandomMorning.length)]}`,
+					content: `${interaction.user.name}${RandomMorning[Math.floor(Math.random() * RandomMorning.length)]}`,
 					ephemeral: false,
 				},
 			);
