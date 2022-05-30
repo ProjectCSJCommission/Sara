ed1c6bfc380c4bed1b3a7dbde1dadee2b6dcd405
YuuJinS <yuujins@DESKTOP-GIA4JKF.localdomain>
2022-05-30 12:26:39 +0900

    add deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,33 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const dotenv = require('dotenv');
+dotenv.config();
+const fs = require('node:fs');
+
+const commands = [];
+const commandFolders = fs.readdirSync('./commands');
+for (const folder of commandFolders) {
+	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`./commands/${folder}/${file}`);
+		commands.push(command.data.toJSON());
+	}
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationCommands(process.env.ClientId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	}
+	catch (error) {
+		console.error(error);
+	}
+})();
