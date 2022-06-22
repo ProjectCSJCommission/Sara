4ef56093e72609ea6ea26d78d516bb33079058aa
音羽みのり <OtohaMinori@users.noreply.github.com>
2022-06-23 03:15:26 +0900

    Add new feature

--- bot.js
+++ bot.js
@@ -17,11 +17,7 @@ const fs = require('node:fs');
 const { Client, Collection, Intents } = require('discord.js');
 const client = new Client({
 	intents: [
-		Intents.FLAGS.DIRECT_MESSAGES,
 		Intents.FLAGS.GUILDS,
-		Intents.FLAGS.GUILD_MESSAGES,
-		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-		Intents.FLAGS.GUILD_VOICE_STATES,
 	],
 });
 
